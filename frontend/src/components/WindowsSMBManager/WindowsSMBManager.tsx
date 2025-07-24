4de85a6d21e27ea3407071eefba911837aaac303
PlogDev <nicolai.singer@plogsties.de>
2025-07-24 14:38:16 +0200

    smbmangaer edited

--- frontend/src/components/WindowsSMBManager/WindowsSMBManager.tsx
+++ frontend/src/components/WindowsSMBManager/WindowsSMBManager.tsx
@@ -347,7 +347,6 @@ const configureSMB = async () => {
                 name="domain"
                 value={configForm.domain}
                 onChange={handleInputChange}
-                required
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="PLOGSTIES"
               />
